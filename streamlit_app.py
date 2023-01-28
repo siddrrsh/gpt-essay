32e6f113ae2d921d3807439b17acb474bc582eb3
Siddharth <siddrrsh@gmail.com>
2023-01-27 21:49:42 -0800

    new stuff.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,26 +1,22 @@
+import pdfplumber
 import streamlit as st
-import pandas as pd
-from io import StringIO
+
+st.write()
+
+def extract_data(feed):
+    data = []
+    with pdfplumber.load(feed) as pdf:
+        pages = pdf.pages
+        for p in pages:
+            data.append(p.extract_tables())
+            st.write(p.extract_tables())
+    return None
+
 
 uploaded_file = st.file_uploader("Choose a file")
-
-fdata = ""
 if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
-
     # To convert to a string based IO:
     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
     st.write(stringio)
+    fdata = stringio
 
-    # To read file as string:
-    string_data = stringio.read()
-    fdata = string_data
-    st.write(string_data)
-
-    # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
-
-print(fdata)
